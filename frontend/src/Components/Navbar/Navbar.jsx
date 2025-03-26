a81cc3774b74be218753755e469548fd6a00d9ef
Kennedy <kenrichshiku@gmail.com>
2025-03-26 17:34:46 +0300

    Created Navbar component

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../Assets/iphone_store.png'
+import { FiShoppingCart } from "react-icons/fi";
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
