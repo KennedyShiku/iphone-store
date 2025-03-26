c1947e8ebfc6688e20df586945add85b1d4bfd0b
Kennedy <kenrichshiku@gmail.com>
2025-03-26 17:56:02 +0300

    Added basic structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='font-outfit'>
       <Navbar />
     </div>
   )
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import logo from '../Assets/iphone_store.png'
+import logo from '../Assets/iphone_store.png';
 import { FiShoppingCart } from "react-icons/fi";
 
 const Navbar = () => {
   return (
     <div>
-      
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>iPhones</li>
+        <li>Laptops</li>
+        <li>Audio</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <FiShoppingCart />
+      </div>
     </div>
   )
 }
