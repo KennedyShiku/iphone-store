43ef42c7a372e9b9973bfa850a3afb9530dcc5dd
Kennedy <kenrichshiku@gmail.com>
2025-03-27 15:21:18 +0300

    Finished basic Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../Assets/iphone_store.png';
 import { FiShoppingCart } from "react-icons/fi";
 import './Navbar.css'
 
 const Navbar = () => {
+    
+    const [menu, setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
       <div className="nav-logo">
         <img src={logo} alt="" />
-        <p>SHOPPER</p>
+        <p>iPhone Store Kenya</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop<hr/></li>
-        <li>iPhones</li>
-        <li>Laptops</li>
-        <li>Audio</li>
+        <li onClick={() => {setMenu("shop")}}>Shop {menu === "shop" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("iphones")}}>iPhones {menu === "iphones" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("laptops")}}>Laptops {menu === "laptops" ? <hr /> : <></>}</li>
+        <li onClick={() => {setMenu("audio")}}>Audio {menu === "audio" ? <hr /> : <></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
-        <FiShoppingCart />
+        <FiShoppingCart size={25}/>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
