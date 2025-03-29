26b5bcf84b4c0dc279d92395b041071d45a1f110
Kennedy <kenrichshiku@gmail.com>
2025-03-29 13:51:07 +0300

    Added data file

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import iphone_13 from './iphone_13.png';
+import iphone_13_pro from './iphone_13_pro.png';
+import iphone_14 from './iphone_14.png';
+import iphone_14_pro from './iphone_14_pro.png';
+
+let data_product = [
+  {
+    id: 1,
+    name: "iPhone 13",
+    image: iphone_13,
+    new_price: 99999.00,
+    old_price: 114999.00,
+  },
+  {
+    id: 2,
+    name: "iPhone 13 Pro",
+    image: iphone_13_pro,
+    new_price: 129999.00,
+    old_price: 144999.00,
+  },
+  {
+    id: 3,
+    name: "iPhone 14",
+    image: iphone_14,
+    new_price: 114999.00,
+    old_price: 129999.00,
+  },
+  {
+    id: 4,
+    name: "iPhone 14 Pro",
+    image: iphone_14_pro,
+    new_price: 144999.00,
+    old_price: 159999.00,
+  }
+];
+
+export default data_product;
